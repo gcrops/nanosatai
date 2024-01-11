58540517c1701d75bbc8682f7fd5fc8fcfd8b94b
Krishna Chaithanya <chaitanyam.be20@uceou.edu>
2024-01-11 18:20:54 -0500

    temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MainPage from "./pages/main";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <div>This is the about page</div>
+            </Route>
+            <Route path="/users">
+              <div>This is the users page</div>
+            </Route>
+            <Route path="/">
+              <div>This is the home page</div>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+      <MainPage></MainPage>
     </div>
   );
 }
---
+++ src/pages/main.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function MainPage() {
+  return (
+    <div>
+      Global Biome Services., a Princeton based IT hub that provides IT
+      Services, specialized in helping clients for Cloud migration , Data
+      Transformation and developing products to provide data driven insights
+      using Machine Learning and big data analytics We help increase ROI and
+      business value for our clients through adapting Converged and Hyper
+      Converged platforms and cloud enablement. GBS has a deep understanding of
+      the advanced technologies and our experienced pool of IT resources mean
+      that we have it in us to serve any client, anywhere on the globe. We
+      deliver the value that clients care about; we provide ideas and solutions
+      tailor-made to meet their needs and produce results of the highest
+      quality.Our clients look forward to us for innovative ideas, expertise,
+      and solutions that help them improve the entire gamut of their business.We
+      believe in total commitment and better results for all our associates
+    </div>
+  );
+}
+
+export default MainPage;
