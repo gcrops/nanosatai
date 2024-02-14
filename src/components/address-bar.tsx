8b405984f4af058500865af671fe9b4ec15d24ad
Krishna Chaithanya <chaitanyam.be20@uceou.edu>
2024-02-14 16:20:53 -0500

    feat(address-bar):added bar with icons

---
+++ src/components/address-bar.tsx
@@ -0,0 +1,55 @@
+import './addres-bar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faFacebookF,
+  faTwitter,
+  faLinkedinIn,
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  faLocationDot,
+  faPhone,
+  faEnvelope,
+} from '@fortawesome/free-solid-svg-icons';
+export function AddressBar() {
+  return (
+    <div className="address">
+      <div className="address-left">
+        <div>
+          <FontAwesomeIcon
+            icon={faLocationDot}
+            size="xl"
+            className="address-icon"
+          />
+          81 Big Oak Road, STE # 134 Morrisville, Philadelphia 19067 USA
+        </div>
+
+        <div>
+          <FontAwesomeIcon icon={faPhone} size="xl" className="address-icon" />
+          (215)-395-6373
+        </div>
+
+        <div>
+          <FontAwesomeIcon
+            icon={faEnvelope}
+            size="xl"
+            className="address-icon"
+          />
+          info@dascase.com
+        </div>
+      </div>
+      <div>
+        <FontAwesomeIcon
+          icon={faFacebookF}
+          size="xl"
+          className="address-icon"
+        />
+        <FontAwesomeIcon icon={faTwitter} size="xl" className="address-icon" />
+        <FontAwesomeIcon
+          icon={faLinkedinIn}
+          size="xl"
+          className="address-icon"
+        />
+      </div>
+    </div>
+  );
+}
--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,17 +1,22 @@
 import cropxLogo from '../assets/cropxnav.png';
 import './nav-bar.css';
-
+import { AddressBar } from './address-bar.tsx';
 export function Navbar() {
   return (
-    <div className="NavFlex">
-      <img src={cropxLogo} alt="logo" className="cropxLogo" />
-      <div className='ham'>
-      <i className="fa fa-bars" aria-hidden="true"></i>
+    <div>
+      <div>
+        <AddressBar />
       </div>
-      <div className="RightFlex">
-        <div>Analytics</div>
-        <div>Consulting</div>
-        <div>Cloud</div>
+      <div className="NavFlex">
+        <img src={cropxLogo} alt="logo" className="cropxLogo" />
+        <div className="ham">
+          <i className="fa fa-bars" aria-hidden="true"></i>
+        </div>
+        <div className="RightFlex">
+          <div>Analytics</div>
+          <div>Consulting</div>
+          <div>Cloud</div>
+        </div>
       </div>
     </div>
   );
